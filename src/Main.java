7a62cf383cbed7d87c2c22aa3333cf024ba9c645
danial <d.bolookat@gmail.com>
2021-08-10 15:56:53 +0430

    create  compelet main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many numbers do you want to enter? ");
+        int size = input.nextInt();
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= size; i++) {
+            System.out.println("enter number " + i + " : ");
+            numbers.add(input.nextInt());
+        }
+        perfectList(numbers);
+
 
     }
     public static void perfectList(ArrayList<Integer> numbers) {
